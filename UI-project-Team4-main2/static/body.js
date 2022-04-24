9616f48ac9d414c64ab610802d8694e40fabbf1a
Leeah Yoo <100104652+leeahyoo@users.noreply.github.com>
2022-04-24 00:20:55 -0400

    First commit

---
+++ UI-project-Team4-main2/static/body.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".previous").click(function(){
+        window.location.href='/';
+    })
+    $(".next").click(function(){
+        window.location.href='/tails';
+    })
+})
